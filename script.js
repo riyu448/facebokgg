51475826c260c3e90e4970f5b13fdfa1153b4e13
riyu448 <rzamoum414@gmail.com>
2025-03-08 16:49:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {  
     document.getElementById("login-form").addEventListener("submit", function (event) {  
         event.preventDefault();  
